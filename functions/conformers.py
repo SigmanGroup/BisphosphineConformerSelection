b9b83cb22a8b42bef5674a3c3d5bf78f24924820
jcadge <j.cadge@icloud.com>
2024-01-31 14:19:39 -0700

    Deleted files

--- functions/conformers.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-def count_conformer_number(path: Path, ligand):
-    all_files = list(path.rglob('*'))
-    
-    count_list = [i for i in all_files if ligand in i.name]
-    count = len(count_list)
-
-    return count
